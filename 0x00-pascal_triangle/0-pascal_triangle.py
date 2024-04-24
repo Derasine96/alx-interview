9d6f919e4fdf9ba38b00cee56fa2f38e3a4a6cc3
Obikwelu Chidera Jane <chidexobikwelu@gmail.com>
2024-04-24 22:05:44 +0000

    Pascal's triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Pascal triangle"""
+
+def pascal_triangle(n):
+    """A function that returns a list of lists of integers representing Pascal’s triangle."""
+    if n <= 0:
+        return []
+    result = [[1]]
+    for i in range(1, n):
+        prev_row = result[-1]
+        new_row = [1]
+        for j in range(1, i):
+            new_row.append(prev_row[j - 1] + prev_row[j])
+        new_row.append(1)
+        result.append(new_row)
+    return result
