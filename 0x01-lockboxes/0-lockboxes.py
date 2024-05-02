55b6c606559d4c181e714043b453d8dd5ac31ce8
Obikwelu Chidera <chidexobikwelu@gmail.com>
2024-05-03 00:03:33 +0100

    Lockboxes - Interview questions

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""a method that determines if all the boxes can be opened."""
+
+
+def canUnlockAll(boxes):
+    """
+    a method that determines if all the boxes can be opened.
+    Args:
+        boxes (list[int]) A list  of integers representing the numbers on the boxes.
+    Return: True or False
+    """
+    if not boxes or type(boxes) is not list:
+        return False
+    unlocked = [0]
+    for n in unlocked:
+        for key in boxes[n]:
+            if key not in unlocked and key < len(boxes):
+                unlocked.append(key)
+    if len(unlocked) == len(boxes):
+        return True
+    return False
